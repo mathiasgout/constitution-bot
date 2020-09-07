8a64926f0613736b25a822762bc97118260afd95
Mathias Gout <mathias.gout@gmail.com>
2020-09-07 21:36:40 +0200

    new extract_articles

--- extract_articles.py
+++ extract_articles.py
@@ -1,17 +1,23 @@
+import requests
+import os
 import pandas as pd
 
+# articles path
+DIR_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
+if os.path.isdir(DIR_PATH) is False:
+    os.mkdir(DIR_PATH)
+
+ARTICLES_PATH = os.path.join(DIR_PATH, "articles.csv")
+
 # data link : https://github.com/Felixaverlant/French-Constitution
-CONSTITUTION_JSON_PATH = "constitution.json"
-ARTICLES_PATH = "articles.csv"
+r = requests.get("https://raw.githubusercontent.com/Felixaverlant/French-Constitution/master/constitution.json")
+j = r.json()
+df = pd.DataFrame.from_dict(j)
 
-df = pd.read_json(CONSTITUTION_JSON_PATH)
-
-
-# Extract articles
+# Extract articles
 def extract_text(article):
     # certains articles ont été abrogés et donc n'ont pas de textes
     return article.get("text", "")
-
 
 articles = df.articles.apply(extract_text)
 
