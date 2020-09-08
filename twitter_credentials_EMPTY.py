42527865e7ad655f5fcc9e04b6d8264aa9b9c4d3
Mathias Gout <mathias.gout@gmail.com>
2020-09-08 10:56:44 +0200

    api keys hidden

--- twitter_credentials_EMPTY.py
+++
@@ -1,7 +0,0 @@
-# Variables that contain the user credentials to acces Twitter API
-# Change them with your keys and rename the file as twitter_credentials.py
-ACCES_TOKEN = 
-ACCES_TOKEN_SECRET = 
-CONSUMER_KEY = 
-CONSUMER_SECRET = 
-
